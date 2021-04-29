9fe66b21d4120a17514d668036b832527a003c17
Chris <cguerrero1@my.wctc.edu>
2021-04-29 14:58:16 -0500

    finished class lab

---
+++ src/main/java/edu/wctc/distjavazodiac/service/BasicMonthListService.java
@@ -0,0 +1,43 @@
+package edu.wctc.distjavazodiac.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.wctc.distjavazodiac.entity.Month;
+import edu.wctc.distjavazodiac.repo.MonthRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BasicMonthListService implements MonthListService {
+
+    @Autowired
+    private MonthRepository monthRepository;
+
+    private List<Month> monthList;
+
+    @Override
+    public List<Month> getMonths() {
+        monthList = new ArrayList<>();
+        monthRepository.findAll().forEach(monthList::add);
+        return monthList;
+    }
+
+//    @PostConstruct
+//    public void initMonths() {
+//        ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            Month[] monthArray = mapper.readValue(Paths.get("months.json").toFile(), Month[].class);
+//            monthList = Arrays.asList(monthArray);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            monthList = new ArrayList<>(0);
+//        }
+//    }
+}
